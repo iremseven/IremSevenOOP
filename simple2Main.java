8b33ecd97ef894bb731cc98650c0d7272e09f9f1
Hp <iremirem201920@gmail.com>
2022-03-08 14:17:54 +0300

    StudentProfLab

--- simple2Main.java
+++ simple2Main.java
@@ -3,12 +3,15 @@ public class simple2Main {
 
         Professor p1;
         Professor p2;
+        Student s1;
 
         p1 = new Professor( "ensar gül");
         p1.teachs();
 
         p2 = new Professor( "raif hoca");
         p2.teachs();
+
+        s1 = new Student(name: "irem seven")
 
     }
 
