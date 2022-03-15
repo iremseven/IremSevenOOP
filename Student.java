a196cd93f42be6bd805c35f479e8ca0f361cb1dd
Hp <iremirem201920@gmail.com>
2022-03-15 14:11:29 +0300

    StudentProfLab

--- Student.java
+++ Student.java
@@ -1,5 +1,5 @@
 public class Student extends Person{
-    String std_name;
+    private String std_name;
 
     Student(String name) {
 
@@ -7,7 +7,12 @@ public class Student extends Person{
 
 
     }
-
+    public void setStd_name(String std_name){
+        this.std_name = std_name;
+    }
+    public String getStd_name(){
+        return std_name;
+    }
     void learns(){
         System.out.println("std" +std_name+ "is learning now");
     }
--- simple2Main.java
+++ simple2Main.java
@@ -4,12 +4,16 @@ public class simple2Main {
         Professor p1;
         Professor p2;
         Student s1;
+        Student s2;
         Person prsn;
 
         p1 = new Professor("Ensar Gül");
         p1.setCountry("Turkey");
         System.out.println("Ensar Gül : " + p1.getCountry());
         p1.teachs();
+        s2 = new Student("");
+        s2.setStd_name("Utku");
+        System.out.println("Name of Student : " +s2.getStd_name());
 
         p2 = new Professor( "Raif Hoca");
         p2.setCountry("Turkey");
